4fd888a3e52dba64d4cb4171483f1c799c682948
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-07-01 12:17:13 +0530

    adding Real-Time-Quiz-Marker

---
+++ mimo/python_project/Real-Time-Quiz-Marker.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+answers = ["C","B", "C", "A", "D", "A", "B", "D", "D", "C"]
+
+responses = ["C", "B", "C", "A", "D", "A", "B", "D", "D", "C"]
+
+if responses[-1] == answers[len(responses) -1]:
+  correct = True
+else:
+  correct = False
+
+if not correct:
+  del responses[-1]
+  print ("Sorry, please try the last question again!")
+elif len(responses) < len(answers):
+  print ('Quiz not complete.')
+  correct = str(len(responses))
+  print ("You're got" + correct + """answers correct so far, 
+          please add an answer for the next question.""")
+else:
+  print ("Well done, you have completed the quiz!")
+
