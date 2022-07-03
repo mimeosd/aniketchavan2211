7389adea861d02c8c949566c7d9fb1d4f9656f47
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-07-03 19:38:55 +0530

    adding Apartment-Hint (SQLite withe Python)

---
+++ mimo/SQL_project/Apartment_Hunt/Apartments-Hunt.py
@@ -0,0 +1,31 @@
+#!/bin/python
+
+import sqlite3
+
+conn = sqlite3.connect('Apartment-Hunt.db')
+
+c = conn.cursor()
+
+# Inserting Values
+apartments = [ 
+		('EcoStay', 30),
+		('The View', 100),
+		('Sunny Place', 53),
+		('The Den', 80),
+	     ]
+#c.executemany("INSERT INTO apartment VALUES (?,?)", apartments)
+
+# Query 
+c.execute("SELECT * FROM apartment ORDER BY square_meters DESC")
+
+
+
+names = c.fetchall()
+
+for name in names:
+  print (name)
+
+
+conn.commit()
+conn.close()
+
