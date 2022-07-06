fb396a6d827269ca3d7a8dc1dfb3959cef46bdb9
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-07-06 08:33:49 +0530

    adding Python Project

---
+++ mimo/python_project/Department-Store.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+floors = {
+
+  "lower": ["clothes", "food"],
+  "upper": ["sports", "shoes"],
+  "basement": ["gardening", "furniture"]
+
+  }
+
+found = False 
+requested = "food"
+current = "upper"
+
+if requested in floors[current]:
+  print ("Yes, on this floor.")
+  found = True
+
+else:
+  for floor in ["lower", "upper", "basement"]:
+    if requested in floors[floor]:
+      print ("On " + floor + " floor.")
+      found = True
+
+if found == False:
+  print ("Sorry, not sold in this store.")
+
---
+++ mimo/python_project/Pizza-Pricer.py
@@ -0,0 +1,28 @@
+#!/bin/python 
+
+menu = ["margherita", "vegetable", "pepperoni"]
+
+margherita = []
+vegetables = ["peppers", "mushrooms", "onions"]
+pepperoni = ["peppers", "chilli", "salami"]
+
+ingredients = {
+		"peppers": 0.8,
+		"mushrooms": 0.5,
+		"onions": 0.6,
+		"chilli": 0,
+		"salami": 1.5,
+		}
+
+choice = vegetables
+
+base_cost = 2.5
+
+extra_cost = 0
+
+for item in choice:
+  extra_cost += ingredients[item]
+
+total_cost = base_cost + extra_cost
+
+print (total_cost)
