f6a919be6cc561cf34e4d905886ae091af665b52
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-07-16 09:18:13 +0530

    Adding Playlist App javascript project

---
+++ mimo/Web_Deveplment/javascript/Playlist_App/Readme.md
@@ -0,0 +1,4 @@
+## Playlist App
+
+ Let's use a variable to keep track of whether a song is already
+ in a playlist or not for a music-streaming wwbsite.
---
+++ mimo/Web_Deveplment/javascript/Playlist_App/script.js
@@ -0,0 +1,7 @@
+var song = "Don't stop me now";
+var included = true;
+console.log("New Entry: " + song);
+if (included) {
+ console.log("Already included!");
+}
+console.log("Save and continue");
