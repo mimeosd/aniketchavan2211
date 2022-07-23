1efa5ad5b523c3fc844b6042d3cb0123e2faebd1
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-07-23 07:46:53 +0530

    Adding Lives Left Game JS project

---
+++ mimo/Web_Deveplment/javascript/Lives_Left_Game/Readme.md
@@ -0,0 +1,4 @@
+## Lives Left Game
+
+ Let's write a program that uses a `lives` variable to keep track
+ of lives left in a video game.
---
+++ mimo/Web_Deveplment/javascript/Lives_Left_Game/script.js
@@ -0,0 +1,9 @@
+var lives = 2;
+console.log("Lives: " + lives);
+
+lives = lives - 1;
+console.log("Lives: " + lives);
+
+lives = lives - 1;
+console.log("Lives: " + lives);
+console.log("Game over!");
