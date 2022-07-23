5b8832cee17610c46d08b419c18715082e236b7b
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-07-23 07:55:13 +0530

    Adding Mutual Friends List JS project

---
+++ mimo/Web_Deveplment/javascript/Mutual_Friends_List/Readme.md
@@ -0,0 +1,4 @@
+## Mutual Friends List
+
+ Let's write a program that displays a list of mutual friends by
+ adding strings to a `friends` variable.
---
+++ mimo/Web_Deveplment/javascript/Mutual_Friends_List/script.js
@@ -0,0 +1,7 @@
+var friends = "Mutual Friends: ";
+
+friends = friends + " Jan,";
+friends = friends + " Brett,";
+friends = friends + " Carly";
+
+console.log(friends);
