045fd1a87b3cad0df362f1e48f7169c1b8e0438d
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-23 17:55:48 +0530

    Hello World python projects

---
+++ Python/Hello World.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+print ("Hello World")
+
+# Print is a Python Built-in Function.
+# alphanumeric data written in round/cured brackets ().
+# value inside single or double quotation marks is string.
