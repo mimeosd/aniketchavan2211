da80c9a63bfd7cd09c76822ce5225e1cd5ecf9a2
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-23 17:48:08 +0530

    Add two Numbers python projects

---
+++ Python/Add Two Numbers.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+# With Pre-denfined Variables
+
+# num1 = 40
+# num2 = 30
+
+# sum = num1 + num2
+
+# print ("The sum of two number is:", sum)
+
+
+# With User-Input(s)
+
+num1 = float(input("Enter first number here: "))
+num2 = float(input("Enter second number here: "))
+
+sum = num1 + num2
+
+print ("The sum of two number is:", sum)
