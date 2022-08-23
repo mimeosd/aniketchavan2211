37fa64060d0a430dab89f718dbff17f2b9a0b644
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-23 18:14:30 +0530

    adding Find & calculate square root

---
+++ Python/Find & Calculate Square Root.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# (Using Exponentiation)
+# num1 = int(input("Enter a number here: "))
+# sr = num1 ** (1/2) # (0.5)
+# print ("The square root of the given number is", num1)
+
+import math
+
+num1 = int(input("Enter a number: "))
+sr = math.sqrt(num1)
+print ("Enter square root of the given number is: ", sr)
