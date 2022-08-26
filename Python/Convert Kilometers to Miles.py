f7dbdb89b51fe55b70e584139a524f06bc489ba6
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-26 21:32:23 +0530

    adding Convert km to miles using python

---
+++ Python/Convert Kilometers to Miles.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Convert Kilometers in Miles using Python
+
+km = float(input("Enter your value in km: "))
+
+miles = km * (0.621371)
+
+print (km, "km will be", miles, "miles")
