f9c294da04f7d3167de22d8ebb7a6374314af84f
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-26 21:59:24 +0530

    adding check if number is positive, negative or zero python

---
+++ Python/Check if number is positive, negative or zero.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+	print("It is positive number ")
+elif num < 0:
+	print("It is negative number ")
+else:
+	print("It is Zero ")
