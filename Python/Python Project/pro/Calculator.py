4c2705f83a90cf1ad0211b44657f174566dfa69e
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-30 11:54:10 +0530

    adding Calculator python

---
+++ Python/Python Project/pro/Calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def calculator(num_1, num_2, op):
+  result = 0
+
+  if op == '+':
+    result = num_1 + num_2
+  elif op == '-':
+    result = num_1 - num_2
+  elif op == '*':
+    result = num_1 * num_2
+  else:
+    print("error ")
+
+  print(f"{num_1} {op} {num_2} = {result}")
+
+calculator(5, 10, "*")
