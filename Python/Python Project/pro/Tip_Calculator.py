0404636dd3ba9d27aae1a8fc268b3faaea72b8f7
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-08-30 11:47:08 +0530

    adding Tip Calculator uaing python project

---
+++ Python/Python Project/pro/Tip_Calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+bill = 20
+tip_percentage = 0.15
+tax_percentage = 0.067
+
+tip = bill * tip_percentage
+print(f"Tip: {tip}")
+
+tax = bill * tax_percentage
+print(f"Tax: {tax}")
+
+total = bill + tip + tax
+print(f"Total: {total}")
