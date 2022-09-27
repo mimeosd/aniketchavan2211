257222c3f1274cb67decf6eed0787bd4626d0a85
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-09-28 00:09:39 +0530

    adding fibonacci sequence using python

---
+++ Python/Projects/Fibonacci Sequence.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+a = 0
+b = 1
+
+num = int(input('Enter a number to obtain fibonacii sequence: '))
+
+if num == 1:
+  print(a)
+
+else:
+  print(a)
+  print(b)
+  for i in range(2, num):
+    c = a + b
+    a = b
+    b = c
+    print(c)
