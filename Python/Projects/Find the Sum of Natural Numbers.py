0ed711e5e74be5378d4268c6c1de1ede65dfb60b
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-09-28 11:28:00 +0530

    adding Find the sum of Natural numbers using python

---
+++ Python/Projects/Find the Sum of Natural Numbers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+num = int(input('Enter a number: '))
+
+if num < 0:
+  print('Please enter positive number')
+
+else:
+  sum = 0
+
+  while num > 0:
+    sum += num
+    num -= 1
+
+  print(sum)
