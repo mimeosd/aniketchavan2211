00121037a26083b469a1cb8881e990dc4f707da5
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-09-28 11:40:22 +0530

    modified Convert image to ascii art using python

--- Python/Styling/Convert Image into ASCII Art.py
+++ Python/Styling/Convert Image into ASCII Art.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
-import pywhatkit as kit
+#import pywhatkit as kit
+#kit.image_to_ascii_art("file_path", 'filename.txt')
 
-kit.image_to_ascii_art("file_path", 'filename.txt')
+import ascii_magic
+
+output = ascii_magic.from_image_file("file_path", columns = 300, char = '#')
+
+ascii_magic.to_terminal(output)
