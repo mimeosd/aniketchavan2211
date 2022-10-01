179767d7eb5acf275dc944b97bc54aa7e2fb51fa
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-10-01 20:49:36 +0530

    adding Find ASCII Value of Character.py using python

---
+++ Python/Projects/Find ASCII Value of Character.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+char = str(input('Enter a Charcter: : '))
+
+print('Please enter length of 1 characterat a once')
+print('Character could be --> abc, ABC, 012, !#*...')
+
+print('The ASCII value of: ', char, 'is', ord(char))
