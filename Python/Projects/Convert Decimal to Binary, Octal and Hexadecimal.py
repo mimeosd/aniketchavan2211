741751ea1b31941e33d82bd885055b4f82a6f31b
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-10-01 20:39:15 +0530

    adding Convert Decimal to Binary, Octal and Hexadecimal.py using python

---
+++ Python/Projects/Convert Decimal to Binary, Octal and Hexadecimal.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+decimal = int(input('Enter a number: '))
+
+print('The conversion of decimal number', decimal, 'is: ')
+print(bin(decimal), ' in binary')
+print(oct(decimal), ' in octal')
+print(hex(decimal), ' in hexadecimal')
