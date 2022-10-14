4c6b4d105ced468ae2099c51dfa4e278d503deb7
Ivan <97809307+mimeosd@users.noreply.github.com>
2022-10-14 08:58:39 +0200

    Updated
    Just for fun.

--- Python/Python Project/Books-Item.py
+++ Python/Python Project/Books-Item.py
@@ -1,11 +1,13 @@
-#!/bin/python
-
 class Book:
-  title = "Harry Potter and the Deathly Hallows"
-  pages = 607
-  number = "7th"
-  genre = "fantasy"
-  print (f"{title} has {pages} pages")
-  print (f"It is the {number} in the series.")
+	def __init__(self) -> None:
+		self.title = "Harry Potter and the Deathly Hallows"
+		self.pages = 607
+		self.number = "7th"
+		self.genre = "fantasy"
 
+	def displaying_info(self):
+		print(f"{self.title} has {self.pages} pages")
+		print(f"It is the {self.number} in the series.")
 
+c1 = Book()
+c1.displaying_info()
